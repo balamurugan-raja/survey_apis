b824ff1d3c770f83641b0a7489b966bca6bb233b
Balamurugan Rajagopal <189526@TCS.com>
2020-06-18 12:32:51 +0100

    Adding CORS extension

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask, jsonify, request, render_template, url_for, session, redirect
 from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_jwt import JWT, jwt_required
+
 import pymongo
 from pymongo import MongoClient
 from pprint import pprint
-from flask_jwt import JWT, jwt_required
+
+import mongoengine
+
 from security import   authenticate, identity
 
-from security import authenticate, identity
+
 from resources.user import UserRegister, UserArray, UserDetails
 from resources.survey import Survey, Surveydata, Surveyresponse, GetSurveyResponses
 from resources.template import Template, TemplateArray, Templatedata
+
 from models.user import UserModel
-import mongoengine
+
 import data.mongo_setup as mongo_setup
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'rb'
+CORS(app)
 api = Api(app)
+
 
 jwt = JWT(app, authenticate, identity)
 
